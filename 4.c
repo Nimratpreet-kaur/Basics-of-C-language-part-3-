13c7d586079c7019b0baead62a391738f7bc6d4c
Nimratpreet kaur <nimratleo5@gmail.com>
2026-06-24 15:08:35 +0530

    practice ques

---
+++ 4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+void square(int n);
+int main()
+{
+    int n;
+    printf("Enter a number you want to square: \n");
+    scanf("%d", &n);
+    square(n);
+    return 0;
+}
+void square(int n)
+{
+    printf("%f", pow(n , 2));
+    //other way
+    //int square = pow(n,2);
+   // printf("Square is %d \n", square);
+}
